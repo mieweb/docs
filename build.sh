0c8292a1a47043b8412c268694071fc41fa7acec
Doug Horner <horner@mieweb.com>
2021-02-23 12:25:50 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 # make sure deps are installed
 ./setup.sh
 
+# init drive if not there
+
+if [ ! -d .wgd ]
+then
+  ../wikiGDrive/dist/main.js init --drive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --dest ./content --link_mode uglyURLs --service_account=wikigdrive.json
+fi
+
 # sync once and time it
-time wikigdrive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --service_account=wikigdrive.json --dest content --link_mode uglyURLs
+time wikigdrive pull
 time hugo --config 'config-wc.toml'
 time hugo --config 'config-eh.toml'
