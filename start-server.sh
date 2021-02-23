e9e158e9b8a75abe909e4b996e799c9f85de1f9b
Doug Horner <horner@mieweb.com>
2021-02-23 23:13:52 +0000

    Added qa-docs configs for common hosting.

---
+++ start-server.sh
@@ -0,0 +1,5 @@
+:
+tmux new-session -d -s wikigdrive 'node "/root/wikiGDrive/dist/main.js" watch --watch_mode=changes --debug=watch'
+tmux new-session -d -s webchart_hugo 'hugo server --baseURL="https://docs-qa.med-web.com/wc" --config 'config-wc.toml' --appendPort=false --port=1313 --liveReloadPort=443'
+tmux new-session -d -s eh_hugo 'hugo server --baseURL="https://docs-qa.med-web.com/eh" --config 'config-eh.toml' --appendPort=false --port=1314 --liveReloadPort=8443'
+
