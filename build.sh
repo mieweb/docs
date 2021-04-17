abeada2a1dbd7c2e52786e1081bea1317d01c578
horner <horner@mieweb.com>
2021-04-17 19:41:25 -0400

    The drive_id is needed for the service account or watch does not work.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 
 if [ ! -d .wgd ]
 then
-  ../wikiGDrive/dist/main.js init --drive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --dest ./content --link_mode uglyURLs --service_account=wikigdrive.json
+  ../wikiGDrive/dist/main.js init --drive_id "0ALfGlL3hJS03Uk9PVA" --drive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --dest ./content --link_mode uglyURLs --service_account=wikigdrive.json
 fi
 
 # sync once and time it
