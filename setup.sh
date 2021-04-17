50339c82d05a1962e6e5baa27327cc5d77966229
horner <horner@mieweb.com>
2021-04-17 19:49:22 -0400

    Fixed setup.sh to install proper dependancies.

--- setup.sh
+++ setup.sh
@@ -8,8 +8,11 @@ then
   (which hugo || brew install node hugo)
   # brew update node hugo
 elif [[ "$OSTYPE" == "msys" ]]
+then
+  echo "unsuported"
 elif [[ "$OSTYPE" == "linux-gnu" ]]
   if [[ -e apt ]]
+  then
     apt install nodejs hugo hugo-extended
   else
     yum install nodejs hugo hugo-extended
@@ -18,4 +21,10 @@ then
   choco install -y nodejs hugo hugo-extended
 fi
 
-npm i -g @mieweb/wikigdrive postcss postcss-cli
+npm -g list postcss-cli || npm i -g postcss-cli
+npm -g list postcss || npm i -g postcss
+
+if [ ! -d ../wikiGDrive ]
+then
+  npm i -g @mieweb/wikigdrive postcss postcss-cli
+fi
