544c5b54894622b5ef6ced21e039d7dd3cb0be2e
horner <horner@mieweb.com>
2021-04-17 20:26:41 -0400

    now will use wikigdrive from git repo if it exists.

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
 # make sure deps are installed
-./setup.sh
+#./setup.sh
+
+wgdcmd() {
+  if [ -f ../wikiGDrive/dist/main.js ]
+  then
+    echo "Using development version of wikiGDrive"
+    echo time ../wikiGDrive/dist/main.js $*
+    time ../wikiGDrive/dist/main.js $*
+  else 
+    time wikigdrive $*
+  fi
+}
 
 # init drive if not there
 
 if [ ! -d .wgd ]
 then
-  ../wikiGDrive/dist/main.js init --drive_id "0ALfGlL3hJS03Uk9PVA" --drive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --dest ./content --link_mode uglyURLs --service_account=wikigdrive.json
+  wgdcmd init --drive_id "0ALfGlL3hJS03Uk9PVA" --drive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --dest ./content --link_mode uglyURLs --service_account=wikigdrive.json
 fi
 
 # sync once and time it
-time wikigdrive pull
+wgdcmd pull
 time hugo --config 'config-wc.toml'
 time hugo --config 'config-eh.toml'
