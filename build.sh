31d492087d5fceafa893364d9d30cc52f9765875
horner <horner@mieweb.com>
2021-05-10 17:36:41 -0400

    Added watch mode to build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,13 @@ then
   wgdcmd init --drive_id "0ALfGlL3hJS03Uk9PVA" --drive "https://drive.google.com/drive/folders/1nwb2j9w6LFZASdr3ouUXPmouw4tynHv-" --dest ./content --link_mode uglyURLs --service_account=wikigdrive.json
 fi
 
-# sync once and time it
-wgdcmd pull
+if [ "$1" == "watch" ]
+then
+   echo "Run: \"./test.sh live\" to build web interface in real time."
+   wgdcmd watch
+else
+   # sync once and time it
+   wgdcmd pull
+fi
 time hugo --config 'config-wc.toml'
 time hugo --config 'config-eh.toml'
