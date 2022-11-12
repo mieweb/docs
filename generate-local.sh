c952a3cd314f3aefee188da97b274a9d9e573e67
Doug Horner <horner@mieweb.com>
2022-11-12 20:33:32 +0000

    made wikigdrive specific toml

--- generate-local.sh
+++
@@ -1,9 +0,0 @@
-echo "Node version"
-node -v
-npm config set color false &&  npm config set progress false
-echo "npm i"
-npm i
-
-echo "Building docs.enterprisehealth.com"
-echo hugo --config 'config-eh.toml' --baseURL "https://docs.enterprisehealth.com" --minify
-hugo --config 'config-eh.toml' --baseURL "https://docs.enterprisehealth.com" --minify
---
+++ generate-wikigdrive.sh
@@ -0,0 +1,10 @@
+echo "Node version"
+node -v
+npm config set color false &&  npm config set progress false
+echo "npm i"
+npm i
+
+echo "Building docs.enterprisehealth.com"
+rm -rf public/*
+echo hugo --config 'wikigdrive.toml' --baseURL $BASE_URL
+hugo --config 'wikigdrive.toml' --baseURL $BASE_URL
