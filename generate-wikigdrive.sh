ad4543ad4216bbec56bbce24ab1352b86401bcb0
Grzegorz Godlewski <gg@gitgis.com>
2024-03-09 13:51:21 +0100

    Add menu generation before hugo

--- generate-wikigdrive.sh
+++ generate-wikigdrive.sh
@@ -2,12 +2,14 @@ echo "Node version"
 node -v
 npm config set color false &&  npm config set progress false
 echo "npm i"
-npm i
+time npm i
+
+cat content/navigation.md | ./navigation2menu.js > config/_default/menu.en.json
 
 echo "Building docs.enterprisehealth.com"
 rm -rf public/*
 echo hugo --config 'wikigdrive.toml' --baseURL $BASE_URL
-hugo --config 'wikigdrive.toml' --baseURL $BASE_URL
+time hugo --config 'wikigdrive.toml' --baseURL $BASE_URL
 
 CODE=$?
 rm -rf .hugo_build.lock
